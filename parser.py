6c6ddabeb8fe28eca445c506bbf27845179cd838
Gabriel Sturtevant <gsturtevant87@gmail.com>
2018-07-16 01:27:41 -0700

    re-enabled automatic file transfer

--- parser.py
+++ parser.py
@@ -18,13 +18,11 @@ def parse_csv_file(file_path, target_directory=''):
 		else:
 			data.append(line)
 
-
-
 	if target_directory != '':
 		filename = file_path.split('/')[-1].split('.')
 		now = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
 		filename[0] = filename[0] + '_' + now
 		filename = '.'.join(filename)
-		# os.system('mv {} {}'.format(file_path, target_directory+filename))
+		os.system('mv {} {}'.format(file_path, target_directory+filename))
 
 	return columns, data
